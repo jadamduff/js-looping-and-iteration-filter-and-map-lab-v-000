c22effd3a7e33bfee3f33985cca8fe58a66e7c4e
Adam Duff <j.adam.duff@gmail.com>
2018-10-24 23:25:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
 function driversWithRevenueOver(drivers, revenue) {
   return drivers.filter(function(driver) {return driver.revenue > revenue})
 }
+
+function driverNamesWithRevenueOver(drivers, revenue) {
+  let filtered = driversWithRevenueOver(drivers, revenue);
+  return filtered.map(function(driver) {return driver.name})
+}
